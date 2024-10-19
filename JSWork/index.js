c73e73d2e09acdf8731741a900a14b5a7c4cd899
ptomer40 <ptomer40@yahoo.com>
2024-10-19 11:21:02 +0530

    Work till Basic JS

---
+++ JSWork/index.js
@@ -0,0 +1,15 @@
+// console.log("Hello World");
+
+// const a=12;
+// if(a>10){
+//     let b=20;
+//     a=40;
+//     console.log("a inside:"+a);
+// }
+// console.log("a outside:"+b);
+function fun(a,b){
+    console.log("Hiiii");
+    return a+b;
+}
+const result=fun();
+console.log(result);
